ba6c30ea116e16daad9faf3e81b7866a378b4fe8
Rio Silver <rjcsilver@gmail.com>
2025-09-10 20:13:19 -0400

    Passed interfact to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import ListGroup from './components/ListGroup';
+import React from "react";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["One", "Two", "Three", "Four"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Numbers" onSelectItem={handleSelectItem} />
     </div>
   );
-};
+}
 
 export default App;
