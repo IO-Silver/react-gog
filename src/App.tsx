b4e3b4f7597356133bec336a6a63ccae3849cbf1
Rio Silver <rjcsilver@gmail.com>
2025-09-16 18:54:42 -0400

    Added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import React, { useState } from "react";
 import Alert from "./components/Alert/Alert";
 import Button from "./components/Button/Button";
+import Form from "./components/Form/Form";
+import "./App.css";
 
 function App() {
-  const [hideAlert, setHideAlert] = useState(true);
-
   return (
     <div>
-      <Alert
-        hidden={hideAlert}
-        onClose={() => {
-          setHideAlert(true);
-        }}
-      >
-        <span>Hello from Alert component with children prop!</span>
-      </Alert>
-      <Button
-        onClick={() => {
-          setHideAlert(!hideAlert);
-        }}
-      >
-        Click Me
-      </Button>
+      <Form></Form>
     </div>
   );
 }
